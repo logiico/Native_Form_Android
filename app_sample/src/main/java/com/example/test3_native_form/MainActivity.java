02dccb97e0363324b24aef91be3df7c02ba54506
programmer <taha.dev554@gmail.com>
2019-06-16 13:21:20 +0430

    first commit 3 abc

--- app_sample/src/main/java/com/example/test3_native_form/MainActivity.java
+++ app_sample/src/main/java/com/example/test3_native_form/MainActivity.java
@@ -24,7 +24,7 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-
+ // abc
     private View btn;
     private static final int FORM_REQUEST_CODE = 294;
     private static final String TAG = "TAG_NATIVE_FORM";
